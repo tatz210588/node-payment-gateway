5ebb6dfcd320e9c669702323fb1fdccf4996680c
Tathagat Saha <tathagat.saha@gmail.com>
2022-07-25 23:03:24 +0530

    tokenconfig

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <Button />
-
-
-
-
+        <Button
+          value="0.1" //price of the item
+          ownerAddress="0xE745C2Ab3A5D7CF284465F9F6c3Cb8E2f6a21fd4"
+          chainId="137"
+        />
       </header>
     </div>
   );
--- src/lib/components/Button.js
+++ src/lib/components/Button.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Select from "react-select";
 import { ethers } from "ethers";
-import { getTokenByChain, TokenInfo } from "./tokenConfig";
+import { getTokenByChain } from "./tokenConfig";
 import PhoneLink from "./PhoneLink.json";
+import toast, { Toaster } from "react-hot-toast";
 
 const Button = (props) => {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedToken, setSelectedToken] = useState(null);
 
   async function payNow(token) {
     await window.ethereum.send("eth_requestAccounts"); // opens up metamask extension and connects Web2 to Web3
     const provider = new ethers.providers.Web3Provider(window.ethereum); //create provider
     const signer = provider.getSigner(); // get signer
-    const network = await provider.getNetwork();
     const tokenContract = new ethers.Contract(
       token.address,
       PhoneLink.abi,
@@ -24,25 +25,27 @@ const Button = (props) => {
 
     if ("null" !== token.address) {
       //for non-native coin
-      const tx = await tokenContract.transfer(props.ownerAddress, etherAmount); //transfers tokens from msg.sender to destination wallet
+      console.log("sds");
+      const tx = await tokenContract
+        .transfer(props.ownerAddress, etherAmount)
+        .catch((e) => {
+          console.log(e.data.message);
+        }); //transfers tokens from msg.sender to destination wallet
       toast("Payment in progress... Please Wait", { icon: "👏" });
-      const receipt = await provider
-        .waitForTransaction(tx.hash, 1, 150000)
-        .then(() => {
-          toast.success("Payment Completed.");
-        });
+      await provider.waitForTransaction(tx.hash, 1, 150000).then(() => {
+        toast.success("Payment Completed.");
+      });
     } else {
       //for native coin
+      console.log("test", token.address);
       const tx = await signer.sendTransaction({
         to: props.ownerAddress, //destination wallet address
         value: etherAmount, // amount of native token to be sent
       });
       toast("Payment in progress... Please Wait", { icon: "👏" });
-      const receipt = await provider
-        .waitForTransaction(tx.hash, 1, 150000)
-        .then(() => {
-          toast.success("Payment Completed.");
-        });
+      await provider.waitForTransaction(tx.hash, 1, 150000).then(() => {
+        toast.success("Payment Completed.");
+      });
     }
   }
 
@@ -53,33 +56,28 @@ const Button = (props) => {
     if (selectedValue) {
       token = e;
     }
-    await payNow(token);
+    setSelectedToken(token);
   };
 
   return (
-    <Select
-      placeholder="Select Your Token"
-      value={selectedOption}
-      options={getTokenByChain(props.chainId)}
-      onChange={handleChange}
-      getOptionLabel={(e) => (
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <img width={30} height={30} alt="" src={e.token_icon} />
-          <span style={{ marginLeft: 15 }}>{e.symbol}</span>
-        </div>
-      )}
-    />
-    // <button
-    //   className={`btn btn-${props.kind} CTA`}
-    //   data-id={props.id}
-    //   type={props.type}
-    //   name={props.name}
-    //   value={props.value}
-    //   disabled={props.disabled}
-    //   onClick={props.handleClick}
-    // >
-    //   <h4>{props.label}</h4>
-    // </button>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Select
+        placeholder="Select Your Token"
+        value={selectedOption}
+        options={getTokenByChain(Number(props.chainId))}
+        onChange={handleChange}
+        getOptionLabel={(e) => (
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <img width={30} height={30} alt="" src={e.token_icon} />
+            <span style={{ marginLeft: 15 }}>{e.symbol}</span>
+          </div>
+        )}
+      />
+      <button className={`btn btn-primary CTA`} onClick={payNow(selectedToken)}>
+        <h4>{props.label}</h4>
+      </button>
+    </>
   );
 };
 export default Button;
